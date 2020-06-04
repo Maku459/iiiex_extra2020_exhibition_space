e97d47c8d48230906aa86b964b10b198bd1a7bb8
FlechaMaker <flechacreador@gmail.com>
2020-06-05 00:54:36 +0900

    Daily.co

---
+++ app/javascript/packs/space_feedback.js
@@ -0,0 +1,15 @@
+import DailyIframe from "@daily-co/daily-js"
+
+let callFrame = DailyIframe.createFrame({
+    iframeStyle: {
+        position: 'fixed',
+        border: '1px solid black',
+        width: '375px',
+        height: '450px',
+        right: '1em',
+        bottom: '1em'
+    },
+    showLeaveButton: true,
+    showFullscreenButton: true
+});
+callFrame.join({ url: 'https://yuchi.daily.co/hello' });
