3134c7b096a71ce65b725961671851fb82981824
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2020-06-14 03:23:06 +0900

    地図モーダル、メガネ未モーダル

--- app/javascript/packs/space_ui.js
+++ app/javascript/packs/space_ui.js
@@ -5,24 +5,34 @@ import modaal from 'modaal';
 $(document).ready(function(){
 	$('#glass__now').on('click',function(){
 		if($('#glass__buttons').css('display') == 'block'){
-			$('#glass__buttons').fadeOut();
+            $('#glass__buttons').fadeOut();
 		}else{
             $('#glass__buttons').fadeIn();
 		}
-	});
-	//e.preventDefalt()でリンク無効にしました
+    });
+
+    /*
+    $('#glass__now').on('click',function(){
+        Swal.fire({
+            html: '<ul class="glass__buttons"><li><a href="#" data-no="0" id="button__normal"><img src="/images/colorglass_transparent.png" alt="" width="80px" height="" border="0" /></a></li><li><a href="#" data-no="1" id="button__red"><img src="/images/colorglass_red.png" alt="" width="80px" height="" border="0" /></a></li><li><a href="#" data-no="2" id="button__green"><img src="/images/colorglass_green.png" alt="" width="80px" height="" border="0" /></a></li><li><a href="#" data-no="3" id="button__blue"><img src="/images/colorglass_blue.png" alt="" width="80px" height="" border="0" /></a></li></ul>'
+        })
+    });
+    */
+
 	$('#glass__buttons a').on('click',function(e){
         e.preventDefault();
         let src = $(this).children('img').attr('src');
         $('#glass__now').children('img').attr('src', src);
+
     });
 
-    $('.inline').on('click',function(e){
-        Swal.fire(
-            'Good job!',
-            'You clicked the button!',
-            'success'
-        )
+    $('#map__launcher').on('click',function(){
+        Swal.fire({
+            width: 800,
+            imageUrl: '/images/map.png',
+            imageWidth: 400,
+            html: '<ul><li>①ぼやける境界</li><li>②気配のふるまい</li></ul>'
+        })
     });
     
 })
