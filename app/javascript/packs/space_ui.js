d65a51d33d7b17e10bd6b6f66e78284b1819c95e
Tomoki Nishida <nisitomosagami@gmail.com>
2020-06-16 00:20:46 +0900

    移動スティックUI実装、移動の反映は未実装

--- app/javascript/packs/space_ui.js
+++ app/javascript/packs/space_ui.js
@@ -26,5 +26,26 @@ $(document).ready(function(){
             html: '<ul><li>①ぼやける境界</li><li>②気配のふるまい</li></ul>'
         })
     });
+
+    $('#stick__cursor').on('touchstart',function(){
+        var clickpos = [];
+        clickpos = event.touches[0];
+        $('#stick__cursor').on('touchmove',function(){
+            var deltaX, deltaY;
+            deltaX = event.touches[0].clientX - clickpos.clientX;
+            deltaY = event.touches[0].clientY - clickpos.clientY;
+            var scale = Math.sqrt(deltaX ** 2 + deltaY ** 2) * 2 / parseFloat($('#stickpad__container').css('width'));
+            deltaX /= Math.max(1, scale);
+            deltaY /= Math.max(1, scale);
+            var obj = $('.stick-cursor').offset();
+            obj.left += deltaX;
+            obj.top += deltaY;
+            $('#stick__cursor').offset(obj);
+        })
+    })
+    
+    $('#stick__cursor').on('touchend',function(){
+        $('#stick__cursor').offset($('.stick-cursor').offset());
+    })
     
 })
