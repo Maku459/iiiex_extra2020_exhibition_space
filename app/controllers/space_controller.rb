000b0e87fc5e26706bb3a627477046b5bc494623
FlechaMaker <flechacreador@gmail.com>
2020-06-17 01:09:56 +0900

    Conohaを参照できるようにした

--- app/controllers/space_controller.rb
+++ app/controllers/space_controller.rb
@@ -4,4 +4,14 @@ class SpaceController < ApplicationController
 
   def feedback1
   end
+
+  def works
+    works_name = ["audioracing", "omiage"]
+
+    unless works_name.include?(params[:name]) then
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    end
+
+    @name = params[:name]
+  end
 end
