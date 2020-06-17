b079717c9e6d05edac6daa4bca3e081ba62ba954
FlechaMaker <flechacreador@gmail.com>
2020-06-17 11:42:28 +0900

    ページ間の遷移をテストするためのリストページを作成

--- app/controllers/space_controller.rb
+++ app/controllers/space_controller.rb
@@ -8,8 +8,15 @@ class SpaceController < ApplicationController
   def works
     works_name = ["audioracing", "omiage"]
 
+    if params[:name] == "list" then
+      @list = works_name
+      render template: 'space/works_list', status: 200, layout: 'application', content_type: 'text/html'
+      return
+    end
+
     unless works_name.include?(params[:name]) then
       render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+      return
     end
 
     @name = params[:name]
