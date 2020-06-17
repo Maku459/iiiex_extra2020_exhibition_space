3a3f03c314408e4b63f8b1d6deaf98c5708988bf
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2020-06-18 03:11:21 +0900

    モーダル部分ほぼ完成

--- app/javascript/packs/space_ui.js
+++ app/javascript/packs/space_ui.js
@@ -1,30 +1,20 @@
-import Swal from 'sweetalert2';
 import 'modaal';
 import 'modaal/dist/css/modaal.css'
 
 $(document).ready(function(){
-	$('#glass__now').on('click',function(){
-		if($('#glass__buttons').css('display') == 'block'){
-            $('#glass__buttons').fadeOut();
-		}else{
-            $('#glass__buttons').fadeIn();
-		}
+    $('.glass__buttons').modaal({
+        content_source: '#glass__buttons'
     });
 
 	$('#glass__buttons a').on('click',function(e){
         e.preventDefault();
         let src = $(this).children('img').attr('src');
-        $('#glass__now').children('img').attr('src', src);
-
+        $('.glass__buttons').children('img').attr('src', src);
+        $('.glass__buttons').modaal('close');
     });
 
-    $('#map__launcher').on('click',function(){
-        Swal.fire({
-            width: 800,
-            imageUrl: '/images/map.png',
-            imageWidth: 400,
-            html: '<ul><li>①ぼやける境界</li><li>②気配のふるまい</li></ul>'
-        })
-    });
-    
+    $('.map__modal').modaal({
+        content_source: '#map__modal',
+        width: 700
+    });   
 })
