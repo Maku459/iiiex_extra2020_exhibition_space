63aa4d25de1ee28632e8589adb250f41da9d7f3a
FlechaMaker <flechacreador@gmail.com>
2020-06-18 17:53:47 +0900

    Enable Heroku Ruby Metrics

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+require 'barnes'
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -36,6 +38,12 @@ preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+before_fork do
+  # worker specific setup
+
+  Barnes.start # Must have enabled worker mode for this to block to be called
+end
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
