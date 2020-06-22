88f6292649aeb304c3b1e80ff13a0af4b1b44cac
imo <imoxxx@gmail.com>
2020-06-22 11:15:55 +0900

    meshwalkを外してモデル読み込みまで

--- app/javascript/packs/space_index.js
+++ app/javascript/packs/space_index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import * as MW from 'meshwalk';
+import * as iNoBounce from './inobounce.min';
 
 let W, H;
 const scene = new THREE.Scene(), renderer = new THREE.WebGLRenderer({alpha: true, antialias: true}), camera = new THREE.PerspectiveCamera(30, W/H, 1, 3000), clock = new THREE.Clock();
@@ -11,22 +11,7 @@ const spFB = 20, spLR = 0.2, spUD = 10, exLR = 1, len = 30;
 let LRs = new Array(), FBs = new Array(), UDs = new Array(), dirs = [false, false, false, false, false, false];
 const pmouse = new THREE.Vector3();
 
-MW.install(THREE);
-const world = new MW.World(), min = new THREE.Vector3(-50, -1, -50), max = new THREE.Vector3(50, 50, 50), partition = 5, playerRadius = 1;
-const octree = new MW.Octree(min, max, partition);
-world.add(octree);
-
-const playerObjectHolder = new THREE.Object3D();
-playerObjectHolder.position.set(0, camY, 0);
-scene.add(playerObjectHolder);
-
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(playerRadius, 16, 16), new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true}));
-playerObjectHolder.add(sphere);
-
-const playerController = new MW.CharacterController(playerObjectHolder, playerRadius);
-world.add(playerController);
-
-
+iNoBounce.enable();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setClearColor(0x000000);
 
@@ -36,71 +21,38 @@ document.addEventListener('DOMContentLoaded', () => {
 	window.addEventListener("orientationchange", setMain);
 	document.querySelector("#world").appendChild(renderer.domElement);
 	
-	camera.position.set(0, camY, 10);
+	camera.position.set(0, camY, 0);
 	pitch = camY;
 	
 	const lights = new THREE.Group();
-	const light0 = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-	light0.position.set(0, 50, 0);
+	const light0 = new THREE.PointLight(0xffffff, 0.8, 0, 0);
+	light0.position.set(0, 150, 0);
 	lights.add(light0);
-	const directionalLightHelper0 = new THREE.DirectionalLightHelper(light0);
-	lights.add(directionalLightHelper0);
-	scene.add(lights);
-	const light1 = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-	light1.position.set(50, 50, 0);
-	lights.add(light1);
-	const directionalLightHelper1 = new THREE.DirectionalLightHelper(light1);
-	lights.add(directionalLightHelper1);
-	scene.add(lights);
-	const light2 = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-	light2.position.set(-50, 50, 0);
-	lights.add(light2);
-	const directionalLightHelper2 = new THREE.DirectionalLightHelper(light2);
-	lights.add(directionalLightHelper2);
-	scene.add(lights);
-	const light3 = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-	light3.position.set(0, 50, 50);
-	lights.add(light3);
-	const directionalLightHelper3 = new THREE.DirectionalLightHelper(light3);
-	lights.add(directionalLightHelper3);
-	scene.add(lights);
-	const light4 = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-	light4.position.set(0, 50, -50);
-	lights.add(light4);
-	const directionalLightHelper4 = new THREE.DirectionalLightHelper(light4);
-	lights.add(directionalLightHelper4);
-	scene.add(lights);
-	const light5 = new THREE.DirectionalLight(0xFFFFFF, 0.2);
-	light5.position.set(0, -50, 0);
-	lights.add(light5);
-	const directionalLightHelper5 = new THREE.DirectionalLightHelper(light5);
-	lights.add(directionalLightHelper5);
 	scene.add(lights);
 	
-	gltfLoader.load('/model/iiiEx_field_only.gltf', (data) => {
-	    const gltf = data;
-	    const obj = gltf.scene;
-//		console.log(obj.children[2]);
-		scene.add(obj.children[2]);
-/*	    for (let i=0; i<obj.children.length; i++) {
-		    console.log(obj.children[i]);
-		    let geometry = new THREE.Geometry();
-//		    console.log(obj.children[i].geometry.attributes.position.array)
-		    for (let j=0; j<obj.children[i].geometry.attributes.position.count; j++) {
-//			    console.log(obj.children[i].geometry.attributes.position.array[j*3])
-			    geometry.vertices.push(new THREE.Vector3(obj.children[i].geometry.attributes.position.array[j*3], obj.children[i].geometry.attributes.position.array[j*3+1], obj.children[i].geometry.attributes.position.array[j*3+2]));
-		    }
-		    let terrain = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true}));
-		    terrain.scale.set(20, 20, 20);
-//		    console.log(terrain);
-			scene.add(terrain);
-	    }*/
-//		obj.scale.set(20, 20, 20);
+	gltfLoader.load('/model/iiiEx_doom.gltf', (data) => {
+	    const gltf = data.scene;
+	    console.log(gltf)
+	    gltf.position.set(0, -30, 0);
+		scene.add(gltf);
+		
+		gltfLoader.load("/model/landmark.gltf", (data) => {
+			const model = data.scene;
+			model.position.set(0, 0, 16);
+			model.scale.set(16, 16, 16);
+			scene.add(model);
+		});
+		for (var i=0; i<3; i++) {
+			loadModel("/model/iiiEx_primitive_bird_niren.gltf");
+			loadModel("/model/iiiEx_primitive_fish_niren.gltf");
+			loadModel("/model/iiiEx_primitive_snake_niren.gltf");
+		}
 	});
+	
 	const geo = new THREE.SphereGeometry(0.1), mat = new THREE.MeshBasicMaterial({color: "#ffff00"});
-	for (var i=0; i<5000; i++) {
+	for (var i=0; i<3000; i++) {
 		const sphere = new THREE.Mesh(geo, mat);
-		sphere.position.set(Math.random()*100-50, Math.random()*100-50, Math.random()*100-50);
+		sphere.position.set(Math.random()*200-100, Math.random()*200-100, Math.random()*200-100);
 		scene.add(sphere);
 	}
 		
@@ -176,13 +128,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 	
-	document.querySelector("#world").addEventListener("mousedown", (e) => {
+	document.querySelector("#world").addEventListener("pointerdown", (e) => {
 		dragging = true;
 		pmouse.x = e.pageX;
 		pmouse.y = e.pageY;
 	});
 	
-	document.querySelector("#world").addEventListener("mousemove", (e) => {
+	document.querySelector("#world").addEventListener("pointermove", (e) => {
 		if (dragging) {
 			const rot = Math.atan2(e.pageY - pmouse.y, e.pageX - pmouse.x);
 			dirLR += -Math.cos(rot) * exLR;
@@ -192,7 +144,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 	
-	document.querySelector("#world").addEventListener("mouseup", (e) => {
+	document.querySelector("#world").addEventListener("pointerup", (e) => {
 		dragging = false;
 	});
 	
@@ -208,6 +160,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 });
+	
+const loadModel = (pass) => {
+	gltfLoader.load(pass, (data) => {
+		const model = data.scene;
+		scene.add(model);
+	});
+}
 
 const setMain = () => {
 	W = document.getElementById("world").clientWidth;
