ec40b61860f9972ce87b0cd7845fa66df28c6b03
FlechaMaker <flechacreador@gmail.com>
2020-06-26 21:53:06 +0900

    daily.coのサンプル

--- app/javascript/packs/admins_dailyco.js
+++ app/javascript/packs/admins_dailyco.js
@@ -1,3 +1,19 @@
 import DailyIframe from "@daily-co/daily-js"
 
-console.log(DailyIframe)
+let callFrame = DailyIframe.createFrame({
+    showLeaveButton: true,
+});
+
+let participants = callFrame.join({ url: 'https://yuchi.daily.co/private' })
+
+$("#remove_user").click( () => {
+    let id = $("#user_to_be_removed").val()
+    callFrame.updateParticipant(id, {
+            eject: true
+    })
+})
+
+setInterval( () => {
+    let participants = callFrame.participants()
+    $("#users_list").html(Object.keys(participants).map( key => `<li>${key}</li>`))
+}, 5000)
