9d1114415068ef9fde2f74cdfb6e5cf4dacd0144
Hirotaka Hiraki <hiraki_uts1@yahoo.co.jp>
2020-07-03 07:16:48 +0900

    fix: souvenir csrf off

--- app/controllers/souvenirs_controller.rb
+++ app/controllers/souvenirs_controller.rb
@@ -1,5 +1,5 @@
 class SouvenirsController < ApplicationController
-  protect_from_forgery with: :null_session
+  protect_from_forgery :except => [:index, :create]
   before_action :set_souvenir, only: [:show, :edit, :update, :destroy]
 
   # GET /souvenirs
