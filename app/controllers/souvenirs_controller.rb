a51a50a7c064c5f3b035229bf97839f58b86e6d4
Hirotaka Hiraki <hiraki_uts1@yahoo.co.jp>
2020-07-02 17:51:25 +0900

    fix: csf token

--- app/controllers/souvenirs_controller.rb
+++ app/controllers/souvenirs_controller.rb
@@ -1,5 +1,5 @@
 class SouvenirsController < ApplicationController
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   before_action :set_souvenir, only: [:show, :edit, :update, :destroy]
 
   # GET /souvenirs
