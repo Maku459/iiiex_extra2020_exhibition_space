d80f6c9b3d0c9862913ffddf03fd5a9a09243eec
FlechaMaker <flechacreador@gmail.com>
2020-07-02 16:36:50 +0900

    Fix: 条件式のミス

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   def basic_auth
     if Rails.env == "production"
       return authenticate_or_request_with_http_basic do |user, pass|
-        user == ENV["BASIC_AUTH_USER"] && ENV["BASIC_AUTH_PASSWORD"]
+        user == ENV["BASIC_AUTH_USER"] && pass == ENV["BASIC_AUTH_PASSWORD"]
       end
     end
   end
